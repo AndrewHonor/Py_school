979d4a6d70cf9e9ed1faf652d4eb9d3cc24fb736
Dandy <AndrewHonorLovMeaLone@gmail.com>
2022-11-23 17:24:03 +0200

    Майже, я близько. до Супер Імені

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-star_letters = ['    *    \n'
+star_letters = [['    *    \n']
                 '   * *   \n'
                 '  *****  \n'
                 ' *     * \n'
@@ -299,6 +299,7 @@ number_of_letters = len(my_name)
 # print(list_of_letters[n])
 star_name = []
 for x in range(5):
-    for i in range(len(my_name)):
-        print(star_letters[i[x]])
-
+    for i in my_name:
+        index_lettet = list_of_letters.find(i)
+        print(star_letters[index_lettet[]],end='')
+    print()
