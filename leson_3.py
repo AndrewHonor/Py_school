f8f6876c84140c7d3722f19a270d2052605c4623
Dandy <AndrewHonorLovMeaLone@gmail.com>
2022-12-01 17:51:43 +0200

    Потрібно зробити гарне виведення другої задачі

--- leson_3.py
+++ leson_3.py
@@ -14,3 +14,19 @@ def aple_task():
                   'яблук.' if 4 < apples_on_miniPeople < 21 or apples_on_miniPeople % 10 == 0 or
                               str(apples_on_miniPeople)[
                                   -1] == '1' else 'яблука.'), f'і {apples % miniPeople} залишиться в кошику.')
+
+
+def school_desks():
+    miniPeople_classrooms = input("Введіть скільки дітей начається в класах математики через пробіл? ").split(" ")
+    classroom = ('першого', 'другого','третього')
+    # print(miniPeople_classrooms)
+    school_desks_classrooms = []
+    for x in range(3):
+        school_desks_classrooms.append(round(int(miniPeople_classrooms[x]) / 2))
+    print("Потрібно закупити",end='')
+    for x in range(3):
+        print(f'{school_desks_classrooms[x]} парт для')
+
+
+# aple_task()
+school_desks()
