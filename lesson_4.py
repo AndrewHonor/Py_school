4a8dd7fdd04aeae4d8e4eeafee0565c7a16e5673
Dandy <AndrewHonorLovMeaLone@gmail.com>
2022-12-03 12:27:05 +0200

    2 задачки зроблені

--- lesson_4.py
+++ lesson_4.py
@@ -1,4 +1,20 @@
-x = int(input())
-y = int(input())
-for n in range(x if x %2 ==0 else x+1, y+1, 2):
-    print(n, end=' ')
+def calc_supper():
+    # Написать калькулятор для простых операций(+,-,*,/,**),
+    print("Супер калькулятор")
+    var_char = input()
+    var_char = eval(var_char)
+    print(var_char)
+
+
+# По данному целому числу N распечатайте все квадраты натуральных чисел, не превосходящие N, в порядке возрастания.
+def square():
+    var_char = int(input(""))
+    for x in range(1,var_char):
+        if x ** 2 >= var_char:
+            break
+        print(x ** 2, end=" ")
+
+
+
+#calc_supper()
+square()
