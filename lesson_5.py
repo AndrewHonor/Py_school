68bae7d81ebace89f48df321145d49fc9f1719a3
Dandy <AndrewHonorLovMeaLone@gmail.com>
2022-12-09 19:45:27 +0200

    Виконую трикутник С

--- lesson_5.py
+++ lesson_5.py
@@ -10,7 +10,7 @@ def range_var():
 
 def delta_a():
     # Малюємо пустий трикутник, це було важко підбирати числа
-    var_nam = int(true_namber('введіть величину трикутника: '))
+    var_nam = int(true_namber('Введіть величину трикутника_A: '))
     for i in range(var_nam + 1):
         for j in range(2 * var_nam + 1):
             if var_nam - j == i or j - var_nam == i or i == var_nam:
@@ -20,10 +20,27 @@ def delta_a():
         print()
 
 def delta_b():
-    pass
+    var_nam = int(true_namber('Введіть величину трикутника_B: '))
+    for i in range(1,var_nam*2,2):
+        print(('*' * i).center(var_nam * 2))
+
+
 def delta_c():
-    pass
+    var_nam = int(true_namber('Введіть величину трикутника_C: '))
+    mass_l_u = [ [' '] * var_nam for i in range(var_nam)]
+    mass_r_u = [ [' '] * var_nam for i in range(var_nam)]
+    mass_l_d = [ [' '] * var_nam for i in range(var_nam)]
+    mass_r_d = [ [' '] * var_nam for i in range(var_nam)]
+    x = '*'
+    y = ' '
+    for i in range(var_nam):
+        for j in range(var_nam):
+            if i >= var_nam - 1 -j:
+                mass[i][j]= x
+    for x in mass:
+        print(*x)
+
 def delta_d():
     pass
 
-delta_a()
+delta_c()
--- test.py
+++ test.py
@@ -1,1 +1,3 @@
 # print(eval(input('>>>>')))
+
+
