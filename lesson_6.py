9c36c321493287d3452aedd1090aaf83ff18a966
Dandy <AndrewHonorLovMeaLone@gmail.com>
2022-12-10 22:03:41 +0200

    Готовлю 6 домашку/ 2 task YES

--- lesson_6.py
+++ lesson_6.py
@@ -1,14 +1,28 @@
+from random import randint
+
+
 def task_1():
-    pass
+    my_list = [randint(1, 120) for _ in range(100)]
+    return [x for x in my_list if x > 100]
+
+
 def task_2():
-    pass
+    my_list = [randint(1, 120) for _ in range(100)]
+    my_results = [x for x in my_list if x > 100]
+    return my_results
+
+
 def task_3():
     pass
+
+
 def task_4():
     pass
+
+
 def task_5():
     pass
+
+
 def task_6():
     pass
-
-
--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from lesson_6 import *
 
-task_1()
-# task_2()
-# task_3()
+# print(task_1())
+# print(task_2())
+task_3()
 # task_4()
 # task_5()
 # task_6()
